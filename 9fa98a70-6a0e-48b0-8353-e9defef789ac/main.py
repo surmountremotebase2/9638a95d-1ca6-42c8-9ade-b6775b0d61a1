aad3c9d99cf19fbdd638dedd5c40adad5b7ce765
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-13 13:17:13 -0400

    User Yassa Fareed saved changes to main.py at 04/13/2024, 17:17:13 UTC time.

--- 9fa98a70-6a0e-48b0-8353-e9defef789ac/main.py
+++ 9fa98a70-6a0e-48b0-8353-e9defef789ac/main.py
@@ -1,1 +1,25 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.data import OHLCV
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["SPY"]
+
+    @property
+    def interval(self):
+        # Depending on your data provider and strategy preference,
+        # this interval could be "1day" for daily checks.
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    # We don't need any additional data apart from the price history,
+    # so we leave the data list empty.
+    @property
+    def data(self):
+        return []
+
+    def run(self, data):
+        # Fetch SPY historical data from the provided
