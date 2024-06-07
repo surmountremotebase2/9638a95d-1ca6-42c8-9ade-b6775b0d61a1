5e012656b155611ee4187a4fa79828540ca0e71b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-07 11:05:32 -0400

    User Yassa Fareed saved changes to main.py at 06/07/2024, 15:05:31 UTC time.

--- 290cf82f-92bf-40d0-bb2b-0d405cea7e77/main.py
+++ 290cf82f-92bf-40d0-bb2b-0d405cea7e77/main.py
@@ -1,1 +1,29 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB, ATR
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+
+   @property
+   def assets(self):
+      return ["TQQQ", "SPY", "UVXY"]
+
+   @property
+   def interval(self):
+      return "1hour"
+
+   def run(self, data):
+      spy_stake = 0
+      d = data["ohlcv"]
+      atr = ATR("TQQQ", d, 40)
+      qqq_stake = 0
+      if len(d)>3 and "13:00" in d[-1]["TQQQ"]["date"] and atr[-1]/d[-1]["TQQQ"]["close"]>0.001:
+         v_shape = d[-2]["TQQQ"]["close"]<d[-3]["TQQQ"]["close"] and d[-1]["TQQQ"]["close"]>d[-2]["TQQQ"]["close"]
+         log(str(v_shape))
+if v_shape:
+            qqq_stake = 1 - 0.005
+         else: spy_stake = 0.5 - 0.005
+      elif len(d)>3:
+         spy_stake = 0.5 - 0.005
+
+      return TargetAllocation({"TQQQ": qqq_stake, "SPY": spy_stake, "UVXY": 0.005})
