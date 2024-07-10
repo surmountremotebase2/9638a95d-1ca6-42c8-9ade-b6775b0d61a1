731e66e3c163a6ea347a606e258aa96aa6642693
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-10 15:07:34 -0400

    User Yassa Fareed saved changes to multiple files files at 07/10/2024, 19:07:34 UTC time.

--- 62ee9d0d-04f1-4fb0-80f8-59729fe980e6/main.py
+++ 62ee9d0d-04f1-4fb0-80f8-59729fe980e6/main.py
@@ -1,1 +1,65 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, SLOPE
+from surmount.data import VOLUME
+
+from surmount.logging import log
+
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ['TQQQ', 'AAPL']
+        self.data_list = []
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    @property
+    def interval(self):
+        return "1day"
+
+    def run(self, data):
+        holdings = data["holdings"]
+        data = data["ohlcv"]
+        resultant = {}
+
+        first_value_0 = RSI(data=data, length=21, ticker="TQQQ")
+        second_value_0 = 50
+        if first_value_0 and second_value_0:
+            condition_0 = (first_value_0[-1] > second_value_0)
+        else:
+            condition_0 = False
+        first_value_1 = SLOPE(data=data, length=3, ticker="TQQQ")
+        second_value_1 = 0
+        if first_value_1 and second_value_1:
+            condition_1 = (first_value_1[-1] < second_value_1)
+        else:
+            condition_1 = False
+        condition = condition_0 and condition_1
+
+        if condition:
+            allocation = {"TQQQ": 1.0}
+        else:
+            allocation = {"TQQQ": 0.1}
+        resultant = {**resultant, **allocation}
+
+        first_value_0 = data[('volume', 'AAPL')][-1]['value']
+        second_value_0 = 100000
+        if first_value_0 and second_value_0:
+            condition_0 = (first_value_0[-1] < second_value_0)
+        else:
+            condition_0 = False
+        condition = condition_0
+
+        if condition:
+            allocation = {"AAPL": 0.5, "TQQQ": 0.5}
+        else:
+            allocation = {}
+        resultant = {**resultant, **allocation}
+
+        return TargetAllocation(allocation)
