b6c46554a7a2b1b51b802704fac62f260edcb3f3
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-10 15:16:54 -0400

    User Yassa Fareed saved changes to multiple files files at 07/10/2024, 19:16:53 UTC time.

--- 3706fbc2-18c8-4f88-87f0-e9b082302df0/main.py
+++ 3706fbc2-18c8-4f88-87f0-e9b082302df0/main.py
@@ -29,13 +29,13 @@ class TradingStrategy(Strategy):
         log(str(data.keys()))
         resultant = {}
 
-        first_value_0 = RSI(data=data, length=21, ticker="TQQQ")
+        first_value_0 = RSI(data=data_ohlcv, length=21, ticker="TQQQ")
         second_value_0 = 50
         if first_value_0 and second_value_0:
             condition_0 = (first_value_0[-1] > second_value_0)
         else:
             condition_0 = False
-        first_value_1 = Slope(data=data, length=3, ticker="TQQQ")
+        first_value_1 = Slope(data=data_ohlcv, length=3, ticker="TQQQ")
         second_value_1 = 0
         if first_value_1 and second_value_1:
             condition_1 = (first_value_1[-1] < second_value_1)
