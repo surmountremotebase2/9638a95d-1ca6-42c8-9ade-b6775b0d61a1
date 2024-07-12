ada12c9951f138f3058879883439a033f912536e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-12 11:27:23 -0400

    User Yassa Fareed saved changes to multiple files files at 07/12/2024, 15:27:23 UTC time.

--- 6d7dddf7-98c4-44bc-9b12-32e3cd527ad1/main.py
+++ 6d7dddf7-98c4-44bc-9b12-32e3cd527ad1/main.py
@@ -7,7 +7,7 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
 
     def __init__(self):
-        self.tickers = 1day
+        self.tickers = "1day"
         self.data_list = [VOLUME('AAPL')]
 
     @property
