2cc4623dcd6dd4f1ee5e50b86593fb5d590b4e4e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-15 15:26:37 -0400

    User Yassa Fareed saved changes to multiple files files at 07/15/2024, 19:26:36 UTC time.

--- d5ae390a-f6a0-4bca-ba6a-d817a19dff81/main.py
+++ d5ae390a-f6a0-4bca-ba6a-d817a19dff81/main.py
@@ -1,1 +1,19 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.ticker = "SPY"
+
+    @property
+    def assets(self):
+        return [self.ticker]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    def run(self, data):
+        uso_allocation = 1.0
+        return TargetAllocation({self.ticker: uso_allocation})
