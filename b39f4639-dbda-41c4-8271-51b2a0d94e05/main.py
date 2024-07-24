16bd4611f842b7ff6735227b1d7b59561c0ef14b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-24 07:31:54 -0400

    User Yassa Fareed saved changes to multiple files files at 07/24/2024, 11:31:53 UTC time.

--- b39f4639-dbda-41c4-8271-51b2a0d94e05/main.py
+++ b39f4639-dbda-41c4-8271-51b2a0d94e05/main.py
@@ -1,1 +1,18 @@
-#Type code here
+from surmount.base_class import Strategy
+from surmount.logging import log
+from surmount.data import Asset 
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self._assets = ['SPY']
+
+    @property
+    def assets(self):
+        return self._assets
+
+    @property 
+    def interval(self):
+        return "1hour"
+
+    def run(self, data):
+        return {'AAPL': 0.5, 'GME': 0.5}
