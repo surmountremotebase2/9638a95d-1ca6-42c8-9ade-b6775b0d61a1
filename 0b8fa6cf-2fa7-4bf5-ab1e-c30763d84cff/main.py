846f85ae8d5ea002a4c6e6bcd32ec4a05880ef36
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-24 07:23:50 -0400

    User Yassa Fareed saved changes to multiple files files at 07/24/2024, 11:23:49 UTC time.

--- 0b8fa6cf-2fa7-4bf5-ab1e-c30763d84cff/main.py
+++ 0b8fa6cf-2fa7-4bf5-ab1e-c30763d84cff/main.py
@@ -1,5 +1,4 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import SMA, MACD, RSI
 from surmount.logging import log
 from surmount.data import Asset 
 
