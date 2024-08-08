6eedb8d65a56b57230815b341c2d1dad3ec73db9
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-08 10:34:22 -0700

    User Yassa Fareed saved changes to multiple files files at 08/08/2024, 17:34:21 UTC time.

--- 6df9bdc9-650a-4381-ab93-878b29e069d5/main.py
+++ 6df9bdc9-650a-4381-ab93-878b29e069d5/main.py
@@ -1,1 +1,50 @@
-#Type code here
+
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.data import ConsumerConfidence, CboeVolatilityIndexVix, CboeNasdaqHundredVolatilityIndex, StickyPriceConsumerPriceIndex, HousePriceIndex
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["AAPL"]
+        self.data_list = [
+            ConsumerConfidence(),
+            CboeVolatilityIndexVix(),
+            CboeNasdaqHundredVolatilityIndex(),
+            StickyPriceConsumerPriceIndex(), 
+            HousePriceIndex()
+            ]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        aapl_stake = 0
+        
+        consumer_confidence = data[("consumer_confidence",)][-1]['value']
+        log("consumer_confidence: " + str(consumer_confidence))
+
+        house_price_index = data[("house_price_index",)][-1]['value']
+        log("house_price_index: " + str(house_price_index))
+
+        sticky_price_consumer_price_index = data[("sticky_price_consumer_price_index",)][-1]['value']
+        log("sticky_price_consumer_price_index: " + str(sticky_price_consumer_price_index))
+
+        cboe_nasdaq_hundred_volatility_index = data[("cboe_nasdaq_hundred_volatility_index",)][-1]['value']
+        log("cboe_nasdaq_hundred_volatility_index: " + str(cboe_nasdaq_hundred_volatility_index))
+
+        cboe_volatility_index_vix = data[("cboe_volatility_index_vix",)][-1]['value']
+        log("cboe_volatility_index_vix: " + str(cboe_volatility_index_vix))
+        
+
+        aapl_stake = 0.5
+        return TargetAllocation({"AAPL": aapl_stake})
+
