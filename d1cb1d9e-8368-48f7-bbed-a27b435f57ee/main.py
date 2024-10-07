2e6769a794f0dde19516a0f9aa686aea7404a707
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-07 20:30:36 +0500

    User Yassa Fareed saved changes to multiple files files at 10/07/2024, 15:30:35 UTC time.

--- d1cb1d9e-8368-48f7-bbed-a27b435f57ee/main.py
+++ d1cb1d9e-8368-48f7-bbed-a27b435f57ee/main.py
@@ -1,1 +1,19 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import BB
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    @property
+    def assets(self):
+        # Strategy applies to gcusd
+        return ["AAPL","AMZN"]
+
+    @property
+    def interval(self):
+        return "1hour"
+
+    def run(self, data):
+        holdings = data["holdings"]
+        data = data["ohlcv"]
+
+        return TargetAllocation({"AAPL": 0.5,"AMZN":0.5})
