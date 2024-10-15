6958112b34be85f40d640a57b67a988f45e4e0ca
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-15 16:20:04 -0400

    User Yassa Fareed saved changes to multiple files files at 10/15/2024, 20:20:03 UTC time.

--- c5f49a61-02c1-466e-8e83-da14b6dbcf7c/main.py
+++ c5f49a61-02c1-466e-8e83-da14b6dbcf7c/main.py
@@ -6,7 +6,7 @@ class TradingStrategy(Strategy):
 
    @property
    def assets(self):
-      return ["VZ"]
+      return ["SCHO"]
 
    @property
    def interval(self):
@@ -15,10 +15,10 @@ class TradingStrategy(Strategy):
    def run(self, data):
       d = data["ohlcv"]
       qqq_stake = 0
-      if len(d)>3 and "13:00" in d[-1]["VZ"]["date"]:
-         v_shape = d[-2]["VZ"]["close"]<d[-3]["VZ"]["close"] and d[-1]["VZ"]["close"]>d[-2]["VZ"]["close"]
+      if len(d)>3 and "13:00" in d[-1]["SCHO"]["date"]:
+         v_shape = d[-2]["SCHO"]["close"]<d[-3]["SCHO"]["close"] and d[-1]["SCHO"]["close"]>d[-2]["SCHO"]["close"]
          log(str(v_shape))
          if v_shape:
             qqq_stake = 1
 
-      return TargetAllocation({"VZ": qqq_stake})
+      return TargetAllocation({"SCHO": qqq_stake})
