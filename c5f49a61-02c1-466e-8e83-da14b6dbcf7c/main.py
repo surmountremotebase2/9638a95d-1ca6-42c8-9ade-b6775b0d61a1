98f630c4c56b32ee17bb813b5f8444d3308e3c1b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-15 15:58:12 -0400

    User Yassa Fareed saved changes to multiple files files at 10/15/2024, 19:58:11 UTC time.

--- c5f49a61-02c1-466e-8e83-da14b6dbcf7c/main.py
+++ c5f49a61-02c1-466e-8e83-da14b6dbcf7c/main.py
@@ -1,1 +1,24 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+
+   @property
+   def assets(self):
+      return ["ORCL"]
+
+   @property
+   def interval(self):
+      return "1min"
+
+   def run(self, data):
+      d = data["ohlcv"]
+      qqq_stake = 0
+      if len(d)>3 and "13:00" in d[-1]["ORCL"]["date"]:
+         v_shape = d[-2]["ORCL"]["close"]<d[-3]["ORCL"]["close"] and d[-1]["ORCL"]["close"]>d[-2]["ORCL"]["close"]
+         log(str(v_shape))
+         if v_shape:
+            qqq_stake = 1
+
+      return TargetAllocation({"ORCL": qqq_stake})
