0658673f7bb1e552b78390b531dcbcff80ed1331
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-15 11:39:58 -0400

    User Yassa Fareed saved changes to multiple files files at 10/15/2024, 15:39:58 UTC time.

--- b23d3cd6-1279-43b6-b484-19339d197954/main.py
+++ b23d3cd6-1279-43b6-b484-19339d197954/main.py
@@ -1,43 +1,24 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import BB
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
 
 class TradingStrategy(Strategy):
-    @property
-    def assets(self):
-        # Strategy applies to gcusd
-        return ["gcusd"]
 
-    @property
-    def interval(self):
-        return "5min"
+   @property
+   def assets(self):
+      return ["ORCL"]
 
-    def run(self, data):
-        holdings = data["holdings"]
-        data = data["ohlcv"]
-        
-        #for i in range(len(data)):
-        #    data[i]["gcusd"]["open"] = data[i]["gcusd"]["close"]
-        
-      
-        if len(data) < 12:
-            # There isn't enough data to calculate Bollinger Bands
-            # log("Not enough data to calculate Bollinger Bands")
-            return TargetAllocation({})
+   @property
+   def interval(self):
+      return "1min"
 
-        gcusd_stake = holdings.get("gcusd", 0)
-        gcusd_bbands = BB("gcusd", data, 12, 1.5)
-        current_price = data[-1]["gcusd"]['close']  # Current price of gcusd
+   def run(self, data):
+      d = data["ohlcv"]
+      qqq_stake = 0
+      if len(d)>3 and "13:00" in d[-1]["ORCL"]["date"]:
+         v_shape = d[-2]["ORCL"]["close"]<d[-3]["ORCL"]["close"] and d[-1]["ORCL"]["close"]>d[-2]["ORCL"]["close"]
+         log(str(v_shape))
+         if v_shape:
+            qqq_stake = 1
 
-        # log(f" {current_price}  {gcusd_bbands['lower'][-1]}   {gcusd_bbands['mid'][-1]}")
-
-        # Buying condition: the price falls below the lower Bollinger Band
-        if gcusd_stake ==0 and current_price < gcusd_bbands['lower'][-1]:
-            # log(f"Buying gcusd - price below lower Bollinger Band. Current price: {current_price}")
-            gcusd_stake = 1  # Buy gcusd
-        # Selling condition: the price moves above the middle Bollinger Band
-        if gcusd_stake >0 and current_price > gcusd_bbands['mid'][-1]:
-            # log(f"Closing gcusd - price above middle Bollinger Band. Current price: {current_price}")
-            gcusd_stake = 0  # Exit position in gcusd
-
-        return TargetAllocation({"gcusd": gcusd_stake})
+      return TargetAllocation({"ORCL": qqq_stake})
