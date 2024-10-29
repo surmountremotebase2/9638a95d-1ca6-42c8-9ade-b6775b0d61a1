8c07200d1aacdd839b8cbb62a5eecd204b47dce0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-29 19:53:47 +0500

    User Yassa Fareed saved changes to multiple files files at 10/29/2024, 14:53:46 UTC time.

--- 1e546a88-2397-4ef6-820b-41bb151861f2/main.py
+++ 1e546a88-2397-4ef6-820b-41bb151861f2/main.py
@@ -10,7 +10,7 @@ class TradingStrategy(Strategy):
 
     @property
     def interval(self):
-        return "1day"
+        return "1hour"
 
     def run(self, data):
         holdings = data["holdings"]
