42af92281917ba791f63d9d04d51d8e2dbeee344
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-02-14 21:29:30 +0500

    User Yassa Fareed saved changes to multiple files files at 02/14/2025, 16:29:29 UTC time.

--- 850f785b-50cc-443f-aafe-b5e0d7199300/main.py
+++ 850f785b-50cc-443f-aafe-b5e0d7199300/main.py
@@ -25,4 +25,4 @@ class TradingStrategy(Strategy):
             target_allocation = 0
         
     
-        return TargetAllocation({"SHOP": gcusd_stake})
+        return TargetAllocation({"SHOP": target_allocation})
