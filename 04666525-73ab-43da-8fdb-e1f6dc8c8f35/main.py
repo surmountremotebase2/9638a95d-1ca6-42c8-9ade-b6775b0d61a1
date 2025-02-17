166a0a62ec8a80e83433573773b21902b7626b4d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-02-17 21:58:41 +0500

    User Yassa Fareed saved changes to multiple files files at 02/17/2025, 16:58:40 UTC time.

--- 04666525-73ab-43da-8fdb-e1f6dc8c8f35/main.py
+++ 04666525-73ab-43da-8fdb-e1f6dc8c8f35/main.py
@@ -1,1 +1,29 @@
-from surmount.data import InsiderTrading
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import BB
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.count = 0
+
+    @property
+    def assets(self):
+        return ["SHOP"]
+
+    @property
+    def interval(self):
+        return "5min"
+
+    def run(self, data):
+        holdings = data["holdings"]
+        data = data["ohlcv"]
+        
+        if self.count % 2 == 0:
+            target_allocation = 1
+        else:
+            target_allocation = 0
+        
+        self.count += 1
+    
+        return TargetAllocation({"SHOP": target_allocation})
