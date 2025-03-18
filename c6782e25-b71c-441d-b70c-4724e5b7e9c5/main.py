08d3b5bf0950969c2c937ddfe998621c7171c4f0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-03-18 19:39:23 +0500

    User Yassa Fareed saved changes to multiple files files at 03/18/2025, 14:39:22 UTC time.

--- c6782e25-b71c-441d-b70c-4724e5b7e9c5/main.py
+++ c6782e25-b71c-441d-b70c-4724e5b7e9c5/main.py
@@ -1,1 +1,35 @@
-https://meet.google.com/nen-ohci-ntf
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+from surmount.data import Asset, InstitutionalOwnership
+from datetime import datetime, timedelta
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ["AAPL", "GOOG"]
+        self.data_list = []
+
+    @property
+    def interval(self):
+        return "1hour"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        goog = [i["GOOG"]["close"] for i in data["ohlcv"]]
+        aapl = [i["AAPL"]["close"] for i in data["ohlcv"]]
+        try:
+            if (goog[-1]/goog[-2] < 1 or aapl[-1]/aapl[-2]<1):
+                return TargetAllocation({})
+            if goog[-1]/goog[-2] > aapl[-1]/aapl[-2]:
+                return TargetAllocation({"AAPL": 1})
+            else:
+                return TargetAllocation({"GOOG": 1})
+        except: pass
+        return TargetAllocation({})
