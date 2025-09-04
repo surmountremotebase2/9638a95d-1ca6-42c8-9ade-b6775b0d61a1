c010970f8e923f844a4913d09f1586a3f8048696
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-09-04 17:41:41 +0500

    User Yassa Fareed saved changes to multiple files files at 09/04/2025, 12:41:40 UTC time.

--- 0d9af672-23a1-4c2d-a85f-e4e068762ef4/main.py
+++ 0d9af672-23a1-4c2d-a85f-e4e068762ef4/main.py
@@ -1,1 +1,29 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import CryptoAltRanking
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        # Add CryptoAltRanking as a data source
+        self.data_list = [CryptoAltRanking()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+        
+
+    def run(self, data):
+        # Get crypto alt ranking data
+        crypto_rankings = data[("crypto_alt_ranking",)]    
+        asset = next(iter(crypto_rankings[-1]['alt_ranking'])) + "USD"   
+        log(f"Trading: {asset}")
+        return TargetAllocation({asset:1})
