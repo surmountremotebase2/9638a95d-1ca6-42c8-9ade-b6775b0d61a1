c41fc22c6f5402cd9f49e739767e14deba9f224f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-10-21 23:34:41 +0500

    User Yassa Fareed saved changes to multiple files files at 10/21/2025, 18:34:40 UTC time.

--- 9cb094b3-5da6-4c06-97f2-2cff0e032c37/main.py
+++ 9cb094b3-5da6-4c06-97f2-2cff0e032c37/main.py
@@ -1,1 +1,26 @@
-#Type code here
+rom surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import TimMoore
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [TimMoore()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+        
+
+    def run(self, data):
+        tim_moore_holdings = data[("tim_moore",)]      
+        log(f"Trading: {tim_moore_holdings}")
+        return TargetAllocation(tim_moore_holdings)
