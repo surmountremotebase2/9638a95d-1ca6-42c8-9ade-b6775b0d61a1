2eb792c635fe9cedb873e12f9af19c7377f68a26
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-02-19 06:56:01 +0500

    User Yassa Fareed saved changes to multiple files files at 02/19/2026, 01:56:00 UTC time.

--- 3e89757d-4534-4992-812b-b0f1c4236401/main.py
+++ 3e89757d-4534-4992-812b-b0f1c4236401/main.py
@@ -1,1 +1,32 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import NDWFirstTrustFocusFive
+from surmount.logging import log
+
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["AAPL", "MSFT"]
+
+        # Build data sources for each ticker
+        self.data_list = [NDWFirstTrustFocusFive()]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        allocation = {}
+
+        ndw_ftrust5 = data.get(("ndw_ftrust5"))
+        log(f"ndw_ftrust5: {ndw_ftrust5}")
+
+
+        return TargetAllocation(allocation)
