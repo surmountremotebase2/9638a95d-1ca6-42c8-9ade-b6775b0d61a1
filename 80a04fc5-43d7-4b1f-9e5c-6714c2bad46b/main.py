49de65af6cbf7e8ffa9478b7f550d5b02294b56b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-02-19 14:04:50 -0700

    User Yassa Fareed saved changes to multiple files files at 02/19/2026, 21:04:49 UTC time.

--- 80a04fc5-43d7-4b1f-9e5c-6714c2bad46b/main.py
+++ 80a04fc5-43d7-4b1f-9e5c-6714c2bad46b/main.py
@@ -1,1 +1,37 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+from surmount.data import NDWFirstTrustFocusFive, NDWFirstTrustFocusFiveTrades
+from datetime import datetime
+
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = []
+        self.data_list = [NDWFirstTrustFocusFive()]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        key = tuple(self.data_list[0])
+        ndw_data = data.get(key)
+
+        if ndw_data and len(ndw_data) > 0:
+            allocations = ndw_data[-1].get("allocations", {})
+            total = sum(allocations.values())
+
+            if total > 0:
+                normalized = {k: v / total for k, v in allocations.items()}
+                self.tickers = list(normalized.keys())
+                return TargetAllocation(normalized)
+
+        return None
