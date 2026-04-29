8620d516df69172e809aceb31816293461e2628f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-04-29 17:29:10 +0500

    User Yassa123 Fareed saved changes to multiple files files at 04/29/2026, 12:29:09 UTC time.

--- 47cb2793-e19b-4b9d-b1ea-2521de8d1e95/main.py
+++ 47cb2793-e19b-4b9d-b1ea-2521de8d1e95/main.py
@@ -1,1 +1,37 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.logging import log
+from .holdings_tracker import get_holdings, possible_tickers
+import datetime
+
+class TradingStrategy(Strategy):
+    first_run = True
+    last_valid_date = "2022-02-18"
+
+    @property
+    def assets(self):
+        return possible_tickers
+    
+    @property
+    def interval(self):
+      return "1day"
+
+    def run(self, data):
+        if self.first_run:
+            holdings = get_holdings("2022-07-31")
+            self.first_run = False
+        else:
+            try:
+                date = data["ohlcv"][-1]["TSLA"]["date"][0:10]
+                self.last_valid_date = date
+                holdings = get_holdings(date)
+            except IndexError:
+                new_date = datetime.datetime.strptime(self.last_valid_date, "%Y-%m-%d")
+                new_date = new_date + datetime.timedelta(days=1)
+                new_date = datetime.datetime.strftime(new_date, "%Y-%m-%d")
+                self.last_valid_date = new_date
+                holdings = get_holdings(self.last_valid_date)
+        
+        if holdings:
+            return TargetAllocation(holdings)
+        else:
+            return None
