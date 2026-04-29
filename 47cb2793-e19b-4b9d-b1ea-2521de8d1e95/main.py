cc36317a5b20084ce8cfc70486ac6159000daccc
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-04-29 17:34:48 +0500

    User Yassa123 Fareed saved changes to multiple files files at 04/29/2026, 12:34:47 UTC time.

--- 47cb2793-e19b-4b9d-b1ea-2521de8d1e95/main.py
+++ 47cb2793-e19b-4b9d-b1ea-2521de8d1e95/main.py
@@ -1,7 +1,28 @@
 from surmount.base_class import Strategy, TargetAllocation
 from surmount.logging import log
-from .holdings_tracker import get_holdings, possible_tickers
-import datetime
+from datetime import datetime
+import datetime as dt
+
+possible_tickers = [
+    "AAPL", "V", "MSFT", "UNH", "TMUS", "CB", "GOOG", "MOH", "JPM", "HD",
+    "SCHW", "TSLA", "TMO", "MMC", "DHR", "ACN", "DG", "AMZN", "NVDA",
+    "XOM", "BRK.B", "CRM"
+]
+
+holdings = {
+    "2022-07-31": {
+        "MSFT": .22, "AAPL": .19, "GOOG": .12, "AMZN": .12, "TSLA": .075,
+        "UNH": .05, "NVDA": .05, "V": .039, "BRK.B": .039, "CRM": .039,
+    },
+    "2023-01-31": {
+        "MSFT": .19, "AAPL": .18, "GOOG": .09, "AMZN": .081, "XOM": .055,
+        "NVDA": .06, "UNH": .05, "V": .039, "BRK.B": .039, "CRM": .039
+    }
+}
+
+def get_holdings(date):
+    return holdings.get(date, None)
+
 
 class TradingStrategy(Strategy):
     first_run = True
@@ -10,28 +31,28 @@ class TradingStrategy(Strategy):
     @property
     def assets(self):
         return possible_tickers
-    
+
     @property
     def interval(self):
-      return "1day"
+        return "1day"
 
     def run(self, data):
         if self.first_run:
-            holdings = get_holdings("2022-07-31")
+            holdings_data = get_holdings("2022-07-31")
             self.first_run = False
         else:
             try:
                 date = data["ohlcv"][-1]["TSLA"]["date"][0:10]
                 self.last_valid_date = date
-                holdings = get_holdings(date)
+                holdings_data = get_holdings(date)
             except IndexError:
-                new_date = datetime.datetime.strptime(self.last_valid_date, "%Y-%m-%d")
-                new_date = new_date + datetime.timedelta(days=1)
-                new_date = datetime.datetime.strftime(new_date, "%Y-%m-%d")
+                new_date = datetime.strptime(self.last_valid_date, "%Y-%m-%d")
+                new_date = new_date + dt.timedelta(days=1)
+                new_date = datetime.strftime(new_date, "%Y-%m-%d")
                 self.last_valid_date = new_date
-                holdings = get_holdings(self.last_valid_date)
-        
-        if holdings:
-            return TargetAllocation(holdings)
+                holdings_data = get_holdings(self.last_valid_date)
+
+        if holdings_data:
+            return TargetAllocation(holdings_data)
         else:
             return None
