c376dd3ef7663b6673592a1845c22f8938c6378c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-07-23 23:02:03 +0500

    User Yassa123 Fareed saved changes to multiple files files at 07/23/2026, 18:02:03 UTC time.

--- b1f77bdd-95a9-496b-883e-50ae59a45868/main.py
+++ b1f77bdd-95a9-496b-883e-50ae59a45868/main.py
@@ -1,10 +1,10 @@
 from surmount.base_class import Strategy, TargetAllocation, backtest
-from surmount.data import InsiderPurchases
+from surmount.data import CongressBuys
 from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.data_list = [InsiderPurchases()]
+        self.data_list = [CongressBuys()]
         self.tickers = []
 
     @property
@@ -20,11 +20,11 @@ class TradingStrategy(Strategy):
         return self.data_list
 
     def run(self, data):
-        insider_purchases_holdings = data[("insider_purchases",)]
+        congress_buys_holdings = data[("congress_buys",)]
         allocations = {"AAPL": 1}
-        if insider_purchases_holdings:
-            alloc_dict = insider_purchases_holdings[-1]['allocations']
-            log(f"Trading: {insider_purchases_holdings[-1]['allocations']}")
+        if congress_buys_holdings:
+            alloc_dict = congress_buys_holdings[-1]['allocations']
+            log(f"Trading: {congress_buys_holdings[-1]['allocations']}")
             allocations = alloc_dict
         log(f"allocations:{allocations}")
         return TargetAllocation(allocations)
