84a18a7cde272afcefa53b2eece7cfd8b63d1dfe
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-08-19 15:07:58 +0500

    User Yassa123 Fareed saved changes to multiple files files at 08/19/2026, 10:07:57 UTC time.

--- 7ea98054-4c10-45a3-a0a2-70b30c463c9c/main.py
+++ 7ea98054-4c10-45a3-a0a2-70b30c463c9c/main.py
@@ -6,7 +6,7 @@ class TradingStrategy(Strategy):
     @property
     def assets(self):
         # Strategy applies to gcusd
-        return ["gcusd"]
+        return ["gcusddd"]
 
     @property
     def interval(self):
